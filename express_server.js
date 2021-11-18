d5f1d0be449dab7a2c0c58e2f294747d2e6cac06
edlopez000 <edlopez000>
2021-11-17 20:57:52 -0500

    Using static middleware to serve static files

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, './')));
+
 app.get('/', (req, res) => {
   res.send('hello');
 });
