6ce746dcd3e8f29c5295276494d9447f33db0aec
edlopez000 <edlopez000>
2021-11-17 20:48:39 -0500

    express_server.js now serves a static html document

--- express_server.js
+++ express_server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello dawg');
+  res.send('hello');
+});
+
+// Needed the path module to serve a static html file like nginx
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, './home.html'));
 });
 
 app.listen(port, () => console.log('Express started on localhost'));
