67fe73e01a7b2dade6f535e771f04de070391db3
edlopez000 <lopez.edward@outlook.com>
2021-11-24 18:41:59 -0500

    Changed file structure

--- server/express_server.js
+++ server/express_server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.get('/', (req, res) => {
   res.send("hello but i'm also testing nodemon");
@@ -11,7 +12,12 @@ app.get('/', (req, res) => {
 
 // Needed the path module to serve a static html file like nginx
 app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, './home.html'));
+  res.sendFile(path.join(__dirname, '../home.html'));
 });
 
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
+});
+
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log('Express started on localhost'));
--- server/node_server.js
+++ server/node_server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -6,4 +7,5 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
+// eslint-disable-next-line no-console
 server.listen(port, () => console.log(`server started on port ${port}`));
